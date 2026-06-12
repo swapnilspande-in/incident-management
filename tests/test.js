e91bc6d8fdbdce67d7b4cb0074236854be9accc0
Swapnil Pande <swapnil.s.pande@gmail.com>
2026-06-12 08:48:38 +0000

    Changes for external service

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,10 @@
 const cds = require('@sap/cds/lib')
 const { default: axios } = require('axios')
-const { GET, POST, DELETE, PATCH, expect } = cds.test(__dirname + '../../')
+// const { GET, POST, DELETE, PATCH, expect } = cds.test(__dirname + '../../')
+const { GET, POST, DELETE, PATCH, expect } = cds.test(__dirname + '../../', '--with-mocks');
 
-axios.defaults.auth = { username: 'alice' }
+// axios.defaults.auth = { username: 'alice' }
+axios.defaults.auth = { username: 'incident.support@tester.sap.com', password: 'initial' }
 
 jest.setTimeout(11111)
 
